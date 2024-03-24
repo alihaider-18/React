b4b5f61c5535fd7ad5cffc0f5835c9e5d37fb9c5
alihaider-18 <ah18contact@gmail.com>
2024-03-25 00:33:56 +0530

    commit

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [length, setLength] = useState(5);
+  const [length, setLength] = useState(9);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
